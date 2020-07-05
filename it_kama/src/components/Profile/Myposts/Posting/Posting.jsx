28eaa8322910f5cfbb8460b9157f8404c0c44540
Alexei001 <alexeiyoyo12@gmail.com>
2020-07-06 01:15:04 +0300

    addClassCompUsers

--- it_kama/src/components/Profile/Myposts/Posting/Posting.jsx
+++ it_kama/src/components/Profile/Myposts/Posting/Posting.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Posting.module.css';
+import photoUsers from '../../../assets/images/usersPhoto.png';
 
 const Posting = (props) => {
     return (
         <div className={style.item}>
             <div className={style.item_icon}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR5Ck-z8-LBBAyAk_zaAXvlsZ5GG97KQpKQqr3g7eotKm_VthZW&usqp=CAU" alt="" />
+                <img src={photoUsers} alt="some_post"/>
             </div>
             <div className={style.item_post}>
                 <div className={style.post_message}>{props.messages}</div>
--- it_kama/src/components/UsersPage/UsersPage.jsx
+++ it_kama/src/components/UsersPage/UsersPage.jsx
@@ -1,78 +1,65 @@
 
 import React from 'react';
 import style from './Users.module.css';
+import * as axios from 'axios';
+import usersPhoto from '../assets/images/usersPhoto.png';
 
-const Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1, name: 'Dmitry',
-                    follow: true,
-                    status: 'I am looking for a job right now',
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuu4dy4fOi7lH5GxvLcdAoyx5Kf5A7EE7mCA&usqp=CAU',
-                    location: { city: 'Minsk', country: 'Belarus' }
-                },
-                {
-                    id: 2, name: 'Svetlana',
-                    follow: false,
-                    status: 'I am so pretty',
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuu4dy4fOi7lH5GxvLcdAoyx5Kf5A7EE7mCA&usqp=CAU',
-                    location: { city: 'Minsk', country: 'Belarus' }
-                },
-                {
-                    id: 3, name: 'Serghei',
-                    follow: true,
-                    status: 'I like foodbal!!',
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuu4dy4fOi7lH5GxvLcdAoyx5Kf5A7EE7mCA&usqp=CAU',
-                    location: { city: 'Kiev', country: 'Ukraine' }
-                },
-                {
-                    id: 4, name: 'Andrew',
-                    follow: false,
-                    status: 'Iam free to help you to..',
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuu4dy4fOi7lH5GxvLcdAoyx5Kf5A7EE7mCA&usqp=CAU',
-                    location: { city: 'Philadelphia', country: 'United States' }
-                }
 
-            ]
-        )}
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                console.log(response.data.items)
+            });
+    }
 
-        const UsersComponents = props.users.map((u) => {
-            return (
-                <div className={style.content} key={u.id}>
 
-                    <div className={style.ava_follow}>
-                        <img src={u.img} />
-                        {(u.follow === true)
-                            ? <button onClick={() => { props.unfollow(u.id) }}>follow</button>
-                            : <button onClick={() => { props.follow(u.id) }}>unfollow</button>}
-                    </div>
 
-                    <div className={style.content_users}>
+    render() {
+                    let UsersComponents = this.props.users.map((u) => {
+                        return (
+                            <div className={style.content} key={u.id}>
+                                <div className={style.ava_follow}>
+                                    <img src={(!u.photos === null)
+                                        ? u.photos
+                                        : usersPhoto
+                                    } />
+                                    {(u.follow === true)
+                                        ? <button onClick={() => { this.props.unfollow(u.id) }}>follow</button>
+                                        : <button onClick={() => { this.props.follow(u.id) }}>unfollow</button>}
+                                </div>
 
-                        <div className={style.name_status}>
-                            <div className={style.name}>{u.name}</div>
-                            <div className={style.status}>{u.status}</div>
-                        </div>
+                                <div className={style.content_users}>
 
-                        <div className={style.location}>
-                            <div className={style.country}>{u.location.country}</div>
-                            <div className={style.city}>{u.location.city}</div>
-                        </div>
-                    </div>
+                                    <div className={style.name_status}>
+                                        <div className={style.name}>{u.name}</div>
+                                        <div className={style.status}>{
+                                            (!u.status === null)
+                                                ? u.status
+                                                : 'some text status'
+                                        }</div>
+                                    </div>
 
-                </div>)
-        });
+                                    <div className={style.location}>
+                                        <div className={style.country}>country</div>
+                                        <div className={style.city}>city</div>
+                                    </div>
+                                </div>
 
-    return (
-        <div>
-            <div className={style.Users_wrapper}>
-                <div className={style.title}>Users</div>
-                {UsersComponents}
-            </div>
-        </div>
-    )
+                            </div>)
+                    });
+
+        return (
+            <div>
+                <div className={style.Users_wrapper}>
+                    <div className={style.title}>Users</div>
+                    {UsersComponents}
+                </div>
+            </div>)
+    }
 }
+
 
 export default Users;
